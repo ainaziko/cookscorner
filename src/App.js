52a3f12b0e6ad478d337442a101ccba41d8c5764
Ainazik Momunalieva <58284183+ainaziko@users.noreply.github.com>
2024-07-14 16:57:36 +0600

    added a splash screen preloader with animation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { Router, Routes, Route } from 'react-router-dom'
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+          <Route path='/' element={<Login/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Preloader from '../components/splash/Preloader'
+
+const Login = () => {
+    return (
+        <div>
+            <Preloader/>
+            <p>Hello</p>
+        </div>
+    )
+}
+
+export default Login;
